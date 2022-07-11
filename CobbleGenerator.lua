2102f114993d8b30cbe4a40e684fbfeaa1a855cb
returnzork <returnzork@users.noreply.github.com>
2022-07-11 12:43:26 -0700

    Only wait with the delay if we have a full inventory slot and cannot put items in the chest

--- CobbleGenerator.lua
+++ CobbleGenerator.lua
@@ -2,7 +2,7 @@ while true do
 	if turtle.detect() and turtle.getItemCount() < 64 then
 		turtle.dig()
 	end
-	while not turtle.dropDown() do
+	while not turtle.dropDown() and turtle.getItemCount() == 64 do
 		os.sleep(15)
 	end
 	os.sleep(1)
